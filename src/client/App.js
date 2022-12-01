7bef460ec3db2ce8bb3e63a16aed9a1602832bf4
JNieuwhuis <j.nieuwhuis@hotmail.com>
2022-12-01 10:26:56 +0100

    config: client v1

---
+++ src/client/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Advent of Code 2022</h1>
+      </header>
+      <main className="App-main">
+        <p>Advent of Code 2022 Challenges!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
